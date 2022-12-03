d1a4e9a7cad1c12c83e6276218798f92cae4e4cb
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-12-03 14:50:45 +0200

    Add eslint rule to ignore unused args and vars start with _

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,14 @@ module.exports = {
 	rules: {
 		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
 		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
 	},
 };
