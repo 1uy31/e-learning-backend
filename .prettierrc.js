10aa0fe0b5d2d0e9310f9c2182025be5242adb09
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-12-03 14:58:27 +0200

    Change prettier to use double quote

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
 	useTabs: true, // Indent lines with tabs instead of spaces.
 	tabWidth: 4, // Specify the number of spaces per indentation-level.
 	printWidth: 120, // Specify the length of line that the printer will wrap on.
-	singleQuote: true, // Use single quotes instead of double quotes.
+	singleQuote: false, // Use single quotes instead of double quotes.
 	/**
 	 * Print trailing commas wherever possible.
 	 * Valid options:
@@ -11,5 +11,5 @@ module.exports = {
 	 *   - "es5" - trailing commas where valid in ES5 (objects, arrays, etc)
 	 *   - "all" - trailing commas wherever possible (function arguments)
 	 */
-	trailingComma: 'es5',
+	trailingComma: "es5",
 };
