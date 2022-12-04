e3432ec73ef915bccfe10acb76477d2531800365
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-12-04 20:26:34 +0200

    Update factories and tests of category connector according to updated wrapper

--- src/tests/_factories.ts
+++ src/tests/_factories.ts
@@ -1,18 +1,42 @@
 import { Factory } from "fishery";
 import { Category, createCategoryConnector } from "@database/categoryConnector";
 import { faker } from "@faker-js/faker";
-import { DatabasePoolConnection } from "slonik";
+import { DatabaseTransactionConnection } from "slonik";
+import { createDiaryConnector, Diary } from "@database/diaryConnector";
 
-export const categoryFactory = Factory.define<Category, { connection: DatabasePoolConnection }>(
+export const categoryFactory = Factory.define<Category, { trx: DatabaseTransactionConnection }>(
 	({ sequence, onCreate, transientParams }) => {
 		onCreate(async (category) => {
-			const connector = await createCategoryConnector(transientParams.connection);
+			const connector = await createCategoryConnector(transientParams.trx);
 			return await connector.create(category);
 		});
 
 		return {
 			id: sequence,
 			name: `Category ${sequence}`,
+			createdAt: faker.date.soon(),
+			updatedAt: null,
+		};
+	}
+);
+
+export const diaryFactory = Factory.define<Diary, { trx: DatabaseTransactionConnection; category?: Category }>(
+	({ sequence, onCreate, transientParams }) => {
+		const sourceUrl = faker.internet.domainName();
+		const categoryId = transientParams.category ? transientParams.category.id : null;
+
+		onCreate(async (diary) => {
+			const connector = await createDiaryConnector(transientParams.trx);
+			return await connector.create({ ...diary, sourceUrl });
+		});
+
+		return {
+			id: sequence,
+			topic: `Topic ${sequence}`,
+			rate: sequence,
+			reviewCount: sequence,
+			sourceUrl: faker.internet.domainName(),
+			categoryId: categoryId,
 			createdAt: faker.date.soon(),
 			updatedAt: null,
 		};
--- src/tests/connectors/category.integration.ts
+++ src/tests/connectors/category.integration.ts
@@ -5,25 +5,82 @@ import test from "ava";
 import { createCategoryConnector } from "@database/categoryConnector";
 import { UniqueIntegrityConstraintViolationError } from "slonik";
 import { integrationTestWrapper } from "@src/tests/_utils";
+import { categoryFactory } from "@src/tests/_factories";
 
-test("Happy", async (t) =>
-	integrationTestWrapper(async (connection) => {
-		const connector = await createCategoryConnector(connection);
-		const firstCategory = await connector.create({ name: "First category" });
-		t.is(firstCategory.name, "First category");
+test("Create - Happy", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		const firstCategory = await connector.create({ name: "Category happy" });
+		t.is(firstCategory.name, "Category happy");
 		t.truthy(firstCategory.createdAt instanceof Date);
 		t.is(firstCategory.updatedAt, null);
 
-		const secondCategory = await connector.create({ name: "Second category" });
+		const secondCategory = await connector.create({ name: "Category unhappy" });
 		t.is(secondCategory.id, firstCategory.id + 1);
 		t.truthy(secondCategory.createdAt.getTime() > firstCategory.createdAt.getTime());
 	}));
 
-test("Name constraint violation", async (t) =>
-	integrationTestWrapper(async (connection) => {
-		const connector = await createCategoryConnector(connection);
-		await connector.create({ name: "First category" });
-		await t.throwsAsync(() => connector.create({ name: "First category" }), {
+test("Create - Name constraint violation", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		await connector.create({ name: "Category duplicated" });
+		await t.throwsAsync(() => connector.create({ name: "Category duplicated" }), {
 			instanceOf: UniqueIntegrityConstraintViolationError,
 		});
+	}));
+
+test("Update - Happy", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		const category = await categoryFactory.create({}, { transient: { trx } });
+
+		t.is(category.updatedAt, null);
+		const updatedCategory = await connector.update({ id: category.id, name: "New name" });
+		t.truthy(updatedCategory?.updatedAt instanceof Date);
+		t.is(updatedCategory?.id, category.id);
+		t.is(updatedCategory?.name, "New name");
+	}));
+
+test("Update - Obj not found", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		await categoryFactory.create({}, { transient: { trx } });
+
+		const updatedCategory = await connector.update({ id: 1000, name: "New name" });
+		t.truthy(updatedCategory === undefined);
+	}));
+
+test("Get by name - Happy", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		const category = await categoryFactory.create({ name: "Category get" }, { transient: { trx } });
+
+		const queriedCategory = await connector.getByName("Category get");
+		t.deepEqual(queriedCategory, category);
+	}));
+
+test("Get by name - Obj not found", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		await categoryFactory.create({ name: "Category get" }, { transient: { trx } });
+
+		const queriedCategory = await connector.getByName("Does not exists");
+		t.truthy(queriedCategory === undefined);
+	}));
+
+test("Delete - Happy", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		const categories = await Promise.all([1, 2, 3].map((_i) => categoryFactory.create({}, { transient: { trx } })));
+		const ids = categories.map((category) => category.id);
+
+		const count = await connector.deleteObjs(ids);
+		t.is(count, 3);
+	}));
+
+test("Delete - Obj does not exist", async (t) =>
+	integrationTestWrapper(async (trx) => {
+		const connector = await createCategoryConnector(trx);
+		const count = await connector.deleteObjs([1, 2, 3]);
+		t.is(count, 0);
 	}));
