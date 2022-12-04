ae03476e3e70151c8a8cf553243ac65b57e496cf
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-12-05 01:46:53 +0200

    Fix test wrapper

--- src/tests/_utils.ts
+++ src/tests/_utils.ts
@@ -5,14 +5,21 @@ type TestFunction = (connection: DatabaseTransactionConnection) => Promise<void>
 
 export const integrationTestWrapper = async (testFunction: TestFunction) => {
 	const db = await getDbPool();
-	db.connect(async (connection) => {
+	await db.connect(async (connection) =>
 		connection
-			.transaction(async (trx) => {
-				await testFunction(trx);
-				/* To rollback transaction at the end of the test. */
-				throw new Error();
+			.transaction(async (trx) =>
+				testFunction(trx)
+					.then(() => {
+						throw new Error("Intentionally rolling back the transaction");
+					})
+					.catch((err) => {
+						throw err;
+					})
+			)
+			.catch((err) => {
+				if (err.message !== "Intentionally rolling back the transaction") {
+					throw err;
+				}
 			})
-			.then((_r) => ({}))
-			.catch((_err) => ({}));
-	}).then((_r) => ({}));
+	);
 };
