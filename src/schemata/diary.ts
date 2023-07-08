087c22a561632ac4d835354c411b1a134ff2d38c
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-07-08 16:31:50 +0300

    Add APIs for creating and getting diary by category

---
+++ src/schemata/diary.ts
@@ -0,0 +1,46 @@
+import { createDiaryService } from "@services/diary";
+
+const diaryService = createDiaryService();
+
+export const diaryTypedef = `
+  type Diary {
+    id: Int
+    topic: String
+    sourceUrl: String
+    rate: Int
+    reviewCount: Int
+    categoryId: Int
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  extend type Query {
+  
+    diaries (
+    	topic: String
+        categoryId: Int
+        categoryName: String
+    ): [Diary]
+    
+  }
+  
+  extend type Mutation {
+  
+    addDiary (
+        topic: String!
+        sourceUrl: String
+        rate: Int
+        categoryId: Int
+    ): Diary
+    
+  }
+`;
+
+export const diaryResolver = {
+	Query: {
+		diaries: diaryService.getByCategorizedTopic,
+	},
+	Mutation: {
+		addDiary: diaryService.create,
+	},
+};
---
+++ src/services/diary.ts
@@ -0,0 +1,47 @@
+import { Diary, createDiaryConnector } from "@database/diaryConnector";
+
+export type DiaryService = {
+	getByCategorizedTopic: (
+		_obj: never,
+		kwargs: { topic?: string; categoryId?: number; categoryName?: string }
+	) => Promise<Readonly<Array<Diary>>>;
+	create: (
+		_obj: never,
+		kwargs: { topic: string; sourceUrl?: string; rate?: number; categoryId?: number }
+	) => Promise<Diary>;
+};
+
+export const createDiaryService = (): DiaryService => {
+	const getByCategorizedTopic = async (
+		_obj: undefined,
+		kwargs: { topic?: string; categoryId?: number; categoryName?: string }
+	) => {
+		const diaryConnector = await createDiaryConnector();
+		const diaries = await diaryConnector.getByCategorizedTopic(
+			kwargs.topic,
+			kwargs.categoryId,
+			kwargs.categoryName
+		);
+		return diaries;
+	};
+
+	const create = async (
+		_obj: undefined,
+		kwargs: { topic: string; sourceUrl?: string; rate?: number; categoryId?: number }
+	) => {
+		const diaryConnector = await createDiaryConnector();
+		const diaryData = {
+			topic: kwargs.topic,
+			sourceUrl: kwargs.sourceUrl || null,
+			rate: kwargs.rate || null,
+			categoryId: kwargs.categoryId || null,
+		};
+		const diary = await diaryConnector.create(diaryData);
+		return diary;
+	};
+
+	return {
+		getByCategorizedTopic,
+		create,
+	};
+};
